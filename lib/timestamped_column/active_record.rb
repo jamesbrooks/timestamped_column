637ddd44cb903089726d8bafac56493aff9649ce
James Brooks <james@gooddogdesign.com>
2012-01-23 14:43:44 +1030

    Fixed bug with inproper namespacing

--- lib/timestamped_column/active_record.rb
+++ lib/timestamped_column/active_record.rb
@@ -1,27 +1,29 @@
-module TimestampedColumn::ActiveRecord
-  extend ActiveSupport::Concern
+module TimestampedColumn
+  module ActiveRecord
+    extend ActiveSupport::Concern
 
-  included do
-    before_save    :update_timestamped_column_times
-    cattr_accessor :timestamped_columns
-  end
-
-  module ClassMethods
-    def timestamped_column(column, opts={})
-      timestamp_column = opts[:column] || "#{column}_updated_at"
-
-      self.timestamped_columns ||= {}
-      self.timestamped_columns[column] = timestamp_column
+    included do
+      before_save    :update_timestamped_column_times
+      cattr_accessor :timestamped_columns
     end
-  end
 
-  module InstanceMethods
-  private
-    def update_timestamped_column_times
-      if self.timestamped_columns && changes.any?
-        self.timestamped_columns.each do |column, timestamp_column|
-          if changes[column.to_s]
-            send "#{timestamp_column}=", Time.zone.now
+    module ClassMethods
+      def timestamped_column(column, opts={})
+        timestamp_column = opts[:column] || "#{column}_updated_at"
+
+        self.timestamped_columns ||= {}
+        self.timestamped_columns[column] = timestamp_column
+      end
+    end
+
+    module InstanceMethods
+    private
+      def update_timestamped_column_times
+        if self.timestamped_columns && changes.any?
+          self.timestamped_columns.each do |column, timestamp_column|
+            if changes[column.to_s]
+              send "#{timestamp_column}=", Time.zone.now
+            end
           end
         end
       end
